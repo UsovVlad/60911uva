664c51f9f9214914002efe3c5a480027e823f676
Влад Усов <vladius86rus@gmail.com>
2024-05-04 03:47:59 +0500

    igrations task

---
+++ database/migrations/2024_05_03_222211_modify_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->nullable();
+            $table->unsignedTinyInteger('is_admin')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Удаление добавленных полей при откате миграции
+            $table->dropColumn('phone');
+            $table->dropColumn('is_admin');
+        });
+    }
+};
